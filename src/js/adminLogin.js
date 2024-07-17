fdb0b8fe245c68c9478687034c555a1278c97378
dencoc <denoff39@email.com>
2024-07-17 12:48:17 +0300

    admin login connected

--- src/js/adminLogin.js
+++ src/js/adminLogin.js
@@ -4,21 +4,29 @@ function createAdminLogin(event){
     const info = {}
     info.email = document.getElementById("email-id").value
     info.password = document.getElementById("password-id").value
+
     fetch(`http://26.145.60.29:3000/api/logIn`, {
         method: "POST",
         body: JSON.stringify(info),
         headers:{
             'Content-Type': 'application/json'
-        }
+        },
     })
-
+    .then((data) => data.json())
+    .then(token => {
+        console.log(token.allow)
+        if(token.allow !=="")
+        window.location.href = "./formTitle.html"
+    })
+    
+    
+        
+    
     // const res = fetch(`http://26.145.60.29:3000/api/logIn`,{
     //     method: 'GET'
     // })
     // console.log(res)
 } 
 
-let sib
 let btn = document.getElementById("btn-admin-login")
 btn.addEventListener("click",sib = createAdminLogin)
-console.log(sib)
